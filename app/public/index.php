abdb35a03bb164d7722169d8b8cbd953b5682565
Kamil Serafin <wa12rior@gmail.com>
2023-06-02 09:23:39 +0200

    demo ssrf

--- app/public/index.php
+++ app/public/index.php
@@ -2,7 +2,7 @@
 
 echo <<<HTML
 <form action="" method="get">
-    <input type="hidden" name="lang" value="lang.php">
+    <input type="hidden" name="lang" value="lang.txt">
     <button type="submit">Load lang</button>
 </form>
 HTML;
